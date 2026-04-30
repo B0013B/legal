8f82f1a55fbc7cb6bb92e1fd8fadc76bafad2b41
B0013B <camerjogsnb@ukr.net>
2026-04-30 16:22:38 +0300

    Add files via upload

--- js/slick.js
+++ js/slick.js
@@ -1,38 +1,73 @@
 
-  $('.slider__item').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 5,
-    arrows: false, 
-    dots: true,    
-    customPaging: (slider, i) => `<span class="custom-dot"></span>`,
-autoplay: true,
-autoplaySpeed: 1500,
-speed: 3000,
-
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          dots: true,
-          centerMode: true,
-          centerPadding: '60px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          dots: true,
-          centerMode: true,
-          centerPadding: '60px',
-          slidesToShow: 1
-        }
-      }
-      
-    ]
-  });
+//   $('.slider__item').slick({
+//     centerMode: true,
+//     centerPadding: '20px',
+//     slidesToShow: 5,
   
+//     arrows: true,
+//     prevArrow: '<button class="slick-prev custom-arrow">‹</button>',
+//     nextArrow: '<button class="slick-next custom-arrow">›</button>',
+  
+//     dots: false,
+//     customPaging: (slider, i) => `<span class="custom-dot"></span>`,
+  
+//     autoplay: true,
+//     autoplaySpeed: 2500,
+// speed: 1000,
+  
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: true, 
+//           dots: true,
+//           centerMode: true,
+//           centerPadding: '20px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: true, 
+//           dots: true,
+//           centerMode: true,
+//           centerPadding: '20px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+  
+$('.slider__item').slick({
+  centerMode: true,
+  centerPadding: '20px',
+  slidesToShow: 5,
 
+  arrows: true,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
+
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  speed: 1000,
+
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        centerMode: true,
+        centerPadding: '20px'
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        centerMode: false
+      }
+    }
+  ]
+});
